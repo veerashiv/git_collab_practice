11a6b8fd8a1cfb15b36b8af2cad3c0b0a447736c
veerashiv <veerashankar46@gmail.com>
2025-04-07 15:38:37 +0530

    string file

---
+++ string.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+		char str[100];
+		int i;
+		printf("enter string :");
+		scanf("%s",str);
+		len=strlen(str);
+		for(i=0;i<len;i++)
+		{
+				for(j=i+1;j<len;j++)
+				{
+						if(str[i]==str[j])
+						{
+								cnt++;
+						}
+				}
+		}
+		printf("the repeated string :%s",str[i]);
+}
+
